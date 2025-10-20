6e23af32df56268eb1fbf43427268708d3c001da
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-10-20 18:31:57 +0300

    game-mod : add a database config
    * Added the database config

--- src/main/java/io/quniq/game/config/DatabaseConfig.java
+++ src/main/java/io/quniq/game/config/DatabaseConfig.java
@@ -17,22 +17,35 @@ public class DatabaseConfig {
         try {
             Configuration config = new Configuration();
             
+            // Получаем переменные окружения с fallback значениями
+            String dbHost = getEnvOrDefault("DB_HOST", "localhost");
+            String dbPort = getEnvOrDefault("DB_PORT", "5432");
+            String dbName = getEnvOrDefault("DB_NAME", "minecraft_messages");
+            String dbUsername = getEnvOrDefault("DB_USERNAME", "minecraft_user");
+            String dbPassword = getEnvOrDefault("DB_PASSWORD", "minecraft_password");
+            
             // Настройки подключения к PostgreSQL (Docker)
             config.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
-            config.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/minecraft_messages");
-            config.setProperty("hibernate.connection.username", "minecraft_user");
-            config.setProperty("hibernate.connection.password", "minecraft_password");
+            config.setProperty("hibernate.connection.url", 
+                String.format("jdbc:postgresql://%s:%s/%s", dbHost, dbPort, dbName));
+            config.setProperty("hibernate.connection.username", dbUsername);
+            config.setProperty("hibernate.connection.password", dbPassword);
             
             // Дополнительные настройки для стабильности
-            config.setProperty("hibernate.connection.pool_size", "5");
+            String poolSize = getEnvOrDefault("HIBERNATE_CONNECTION_POOL_SIZE", "5");
+            config.setProperty("hibernate.connection.pool_size", poolSize);
             config.setProperty("hibernate.connection.autocommit", "false");
             config.setProperty("hibernate.connection.provider_disables_autocommit", "true");
             
-            // Настройки Hibernate
-            config.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-            config.setProperty("hibernate.hbm2ddl.auto", "update");
-            config.setProperty("hibernate.show_sql", "true");
-            config.setProperty("hibernate.format_sql", "true");
+            // Настройки Hibernate из переменных окружения
+            config.setProperty("hibernate.dialect", 
+                getEnvOrDefault("HIBERNATE_DIALECT", "org.hibernate.dialect.PostgreSQLDialect"));
+            config.setProperty("hibernate.hbm2ddl.auto", 
+                getEnvOrDefault("HIBERNATE_HBM2DDL_AUTO", "update"));
+            config.setProperty("hibernate.show_sql", 
+                getEnvOrDefault("HIBERNATE_SHOW_SQL", "true"));
+            config.setProperty("hibernate.format_sql", 
+                getEnvOrDefault("HIBERNATE_FORMAT_SQL", "true"));
             
             // Регистрация Entity
             config.addAnnotatedClass(MessageEntity.class);
@@ -68,5 +81,16 @@ public class DatabaseConfig {
         if (entityManagerFactory != null) {
             entityManagerFactory.close();
         }
+    }
+    
+    /**
+     * Получить переменную окружения или значение по умолчанию
+     * @param key ключ переменной окружения
+     * @param defaultValue значение по умолчанию
+     * @return значение переменной или значение по умолчанию
+     */
+    private static String getEnvOrDefault(String key, String defaultValue) {
+        String value = System.getenv(key);
+        return value != null ? value : defaultValue;
     }
 }
