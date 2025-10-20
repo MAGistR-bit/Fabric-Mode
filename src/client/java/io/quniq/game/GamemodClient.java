f7dd748acd2b1c96be57b359d1028f6f228bcf48
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-10-20 16:54:03 +0300

    game-mod : edit a game mod client
    * Edited a game mod client

--- src/client/java/io/quniq/game/GamemodClient.java
+++ src/client/java/io/quniq/game/GamemodClient.java
@@ -1,10 +1,31 @@
 package io.quniq.game;
 
+import io.quniq.game.gui.CustomScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import org.lwjgl.glfw.GLFW;
 
 public class GamemodClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+
+    private static KeyMapping openScreenKey;
+
+    @Override
+    public void onInitializeClient() {
+        openScreenKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key.game_mod.open_screen",
+                GLFW.GLFW_KEY_M,
+                "key.categories.misc"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openScreenKey.consumeClick()) {
+                if (client.player != null) {
+                    Minecraft.getInstance().setScreen(new CustomScreen());
+                }
+            }
+        });
+    }
 }
